4caca60fbd662eee5cce313932623015d4574b13
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-16 18:34:17 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,12 +1,13 @@
 def find_max_value(array)
-  index = 0
-  max_value = -1
-
-  while index < array.length do
-    if max_value < array[index]
-      max_value = array[index]
-    end
-    index += 1
-  end
-  max_value
+  # index = 0
+  # max_value = -1
+  #
+  # while index < array.length do
+  #   if max_value < array[index]
+  #     max_value = array[index]
+  #   end
+  #   index += 1
+  # end
+  # max_value
+  array.max
 end
